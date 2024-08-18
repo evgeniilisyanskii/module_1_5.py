132cf639ca38aa1bcd780deed318aad5585b1f86
evgeniilisyanskii <evgeniilisyanskii@yandex.ru>
2024-08-18 23:37:24 +1000

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = (1,2,3,'a','s','d')
+print( immutable_var)
+mutable_list = [1,2,3,'a','s','d']
+print(mutable_list)
+mutable_list[0] = 500
+print(mutable_list)
+mutable_list.append('Лампа')
+print(mutable_list)
